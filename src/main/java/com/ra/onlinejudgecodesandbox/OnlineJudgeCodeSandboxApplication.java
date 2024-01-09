a8ceae29751b70f02b607b2cf110b702c61bd1d7
jieniyu <jieni.yu42@gmail.com>
2024-01-09 19:36:58 +0100

    initial commit

---
+++ src/main/java/com/ra/onlinejudgecodesandbox/OnlineJudgeCodeSandboxApplication.java
@@ -0,0 +1,13 @@
+package com.ra.onlinejudgecodesandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineJudgeCodeSandboxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineJudgeCodeSandboxApplication.class, args);
+    }
+
+}
